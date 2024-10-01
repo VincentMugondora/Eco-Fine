f62f85d19695da6b854ab16f85a1276237d47316
VincentMugondora <vinmugondora@gmail.com>
2024-10-01 09:30:59 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <button className='btn'>vincent</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/home/home.jsx
@@ -0,0 +1,11 @@
+
+
+const home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default home
