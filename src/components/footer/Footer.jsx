6df003bc10bd6bb5c656b0501f948308184382d8
VincentMugondora <vinmugondora@gmail.com>
2024-10-25 10:57:25 +0200

    created FooterTop.jsx component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,52 +1,13 @@
-import { MdLocationPin } from "react-icons/md";
-import { LuClock4 } from "react-icons/lu";
+import FooterTop from "./footer components/FooterTop";
+
 
 const Footer = () => {
   return (
-    <section className="bg-[#1D1C17] pb-2 mt-96 relative">
+    <section className="bg-[#1D1C17] pb-2 mt-64 relative">
       <section className="container-fluid bg-[#24231D]">
-        <div className="container flex justify-center py-5 px-3 footer absolute -mt-28">
-          <div className="md:flex flex-wrap w-full justify-between">
-            <div className="flex gap-4">
-              <div className="h-20 w-20 flex items-center justify-center rounded-md bg-white">
-                <MdLocationPin className="text-[#00B150] text-3xl" />
-              </div>
-              <div className="text-white">
-                <h5>Location</h5>
-                <div className="">
-                  <p>258 Daniel Mansion 258</p>
-                  <p className="-mt-5">Berlin Germany</p>
-                </div>
-              </div>
-            </div>
-            <div className="flex gap-4">
-              <div className="h-20 w-20 flex items-center justify-center rounded-md bg-white">
-                <LuClock4 className="text-[#00B150] text-3xl" />
-              </div>
-              <div className="text-white">
-                <h5>Working Hours</h5>
-                <div className="">
-                  <p>Weekdays 8am-22pm</p>
-                  <p className="-mt-5">Weekend 10am -12pm</p>
-                </div>
-              </div>
-            </div>
-            <div className="flex gap-4">
-              <div className="h-20 w-20 flex items-center justify-center rounded-md bg-white">
-                <MdLocationPin className="text-[#00B150] text-3xl" />
-              </div>
-              <div className="text-white">
-                <h5>Contact Us</h5>
-                <div className="">
-                  <p>ecofineinfo@ecofine.com</p>
-                  <p className="-mt-5">(+259)2257 6156</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-52">
-            <p>vincent</p>
+        <FooterTop />
+        <div className="">
+          
         </div>
       </section>
       <div className="bg-[#1D1C17] poppins-light text-white container-fluid">
---
+++ src/components/footer/footer components/FooterTop.jsx
@@ -0,0 +1,52 @@
+import { MdLocationPin } from "react-icons/md";
+import { LuClock4 } from "react-icons/lu";
+import { IoIosMail } from "react-icons/io";
+
+const FooterTop = () => {
+  return (
+    <div>
+      <div className="footer md:w-[90%] absolute left-1/2 transform -translate-x-1/2 py-5 px-3 -mt-24">
+        <div className="md:flex flex-wrap w-full justify-between">
+          <div className="flex gap-4">
+            <div className="h-20 w-20 flex items-center justify-center rounded-md bg-white">
+              <MdLocationPin className="text-[#00B150] text-3xl" />
+            </div>
+            <div className="text-white">
+              <h5>Location</h5>
+              <div className="">
+                <p>258 Daniel Mansion 258</p>
+                <p className="-mt-5">Berlin Germany</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <div className="h-20 w-20 flex items-center justify-center rounded-md bg-white">
+              <LuClock4 className="text-[#00B150] text-3xl" />
+            </div>
+            <div className="text-white">
+              <h5>Working Hours</h5>
+              <div className="">
+                <p>Weekdays 8am-22pm</p>
+                <p className="-mt-5">Weekend 10am -12pm</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <div className="h-20 w-20 flex items-center justify-center rounded-md bg-white">
+              <IoIosMail className="text-[#00B150] text-3xl" />
+            </div>
+            <div className="text-white">
+              <h5>Contact Us</h5>
+              <div className="">
+                <p>ecofineinfo@ecofine.com</p>
+                <p className="-mt-5">(+259)2257 6156</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterTop;
